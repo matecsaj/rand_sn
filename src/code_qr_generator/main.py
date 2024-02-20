36277c5b28c062205db1c419c872105a7de303ea
matecsaj <matecsaj@gmail.com>
2024-02-20 00:53:10 -0500

    Saved the list of codes to a json file. Also stubbed in the bar and QR generators.

--- src/code_qr_generator/main.py
+++ src/code_qr_generator/main.py
@@ -1,8 +1,40 @@
 # python
 import argparse
+import json
+import os
+
 from batch import Batch
 from config import Config
 from full_cycle_random import FullCycleRandom
+
+
+def generate_barcode(number: int, path: str) -> None:
+    """
+    Generate a barcode.
+
+    Args:
+        number (int): the number that should be in the barcode
+        path (str): the path where the barcode should be stored
+
+    Returns:
+        None
+    """
+    ...     # TODO
+
+
+def generate_qrcode(number: int, path: str, prefix:str) -> None:
+    """
+    Generate a barcode.
+
+    Args:
+        number (int): the number that should be in the barcode
+        prefix (str): what should be put before the number in the QR code, example, https://mydomain.com/c/.
+        path (str): the path where the barcode should be stored
+
+    Returns:
+        None
+    """
+    ...     # TODO
 
 
 def main() -> None:
@@ -14,17 +46,27 @@ def main() -> None:
 
     batch: Batch or None = None
     try:
+        # preparation
         config = Config()
         fcr = FullCycleRandom(seed=config.seed, min_int=config.min_int, max_int=config.max_int)
         batch = Batch()
 
-        for _ in range(args.count):
-            code = next(fcr, None)
-            if code is None:
-                print("Reached the end of the FullCycleRandom sequence.")
-                break
-            config.seed = code
-            print(f"code {code}")
+        # get the next codes in the sequence, and the new seed
+        codes: list[int] = [next(fcr) for _ in range(args.count)]
+        seed = codes[-1]
+        codes = sorted(codes)
+
+        # store all codes into a file; located at the start of the batch's directory
+        with open(os.path.join(batch.path_directory, 'codes.json'), 'w') as f:
+            json.dump(codes, f, indent=4)
+
+        # for each code generate a barcode image, collection located in the middle
+        for code in codes:
+            generate_barcode(code, batch.path_directory)
+
+        # for each code generate a barcode image, collection located at the end
+        for code in codes:
+            generate_qrcode(code, batch.path_directory, prefix='https://mydomain.com/c/')
 
     except Exception as e:
         if isinstance(batch, Batch):
@@ -32,6 +74,7 @@ def main() -> None:
         raise
 
     else:
+        config.seed = seed
         config.save()
         print(f"Batch {batch.number} generated {args.count} codes.")
 
